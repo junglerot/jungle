enso-org/enso

commit dc30e44b60ba8d4755f0024bcc5e14ae71d7084e

Register instruments/language in their own compilation units to fix the sbt build issues (#3509)
New plan to [fix the `sbt` build](https://www.pivotaltracker.com/n/projects/2539304/stories/182209126) and its annoying:
```
log.error(
"Truffle Instrumentation is not up to date, " +
"which will lead to runtime errors\n" +
"Fixes have been applied to ensure consistent Instrumentation state, " +
"but compilation has to be triggered again.\n" +
"Please re-run the previous command.\n" +
"(If this for some reason fails, " +
s"please do a clean build of the $projectName project)"
)
```
When it is hard to fix `sbt` incremental compilation, let's restructure our project sources so that each `@TruffleInstrument` and `@TruffleLanguage` registration is in individual compilation unit. Each such unit is either going to be compiled or not going to be compiled as a batch - that will eliminate the `sbt` incremental compilation issues without addressing them in `sbt` itself.

fa2cf6a33ec4a5b2e3370e1b22c2b5f712286a75 is the first step - it introduces `IdExecutionService` and moves all the `IdExecutionInstrument` API up to that interface. The rest of the `runtime` project then depends only on `IdExecutionService`. Such refactoring allows us to move the `IdExecutionInstrument` out of `runtime` project into independent compilation unit.

build.sbt
@@ -260,6 +260,11 @@ lazy val enso = (project in file("."))
     searcher,
     launcher,
     downloader,
+    `runtime-language-epb`,
+    `runtime-instrument-id-execution`,
+    `runtime-instrument-repl-debugger`,
+    `runtime-instrument-runtime-server`,
+    `runtime-with-instruments`,
     `runtime-version-manager`,
     `runtime-version-manager-test`,
     editions,
@@ -1156,6 +1161,30 @@ lazy val frgaalJavaCompilerSetting = Seq(
   Compile / javacOptions ++= Seq("-source", frgaalSourceLevel)
 )
 
+
+lazy val instrumentationSettings = frgaalJavaCompilerSetting ++ Seq(
+  version := ensoVersion,
+  commands += WithDebugCommand.withDebug,
+  Compile/logManager :=
+    sbt.internal.util.CustomLogManager.excludeMsg("Could not determine source for class ", Level.Warn),
+  Compile / javacOptions --= Seq("-source", frgaalSourceLevel),
+  libraryDependencies ++= Seq(
+    "org.graalvm.truffle" % "truffle-api"           % graalVersion      % "provided",
+    "org.graalvm.truffle" % "truffle-dsl-processor" % graalVersion      % "provided",
+  ),
+  (Compile / javacOptions) ++= Seq(
+    "-s",
+    (Compile / sourceManaged).value.getAbsolutePath,
+    "-Xlint:unchecked",
+  ),
+)
+
+lazy val `runtime-language-epb` = (project in file("engine/runtime-language-epb"))
+  .settings(
+    inConfig(Compile)(truffleRunOptionsSettings),
+    instrumentationSettings
+  )
+
 lazy val runtime = (project in file("engine/runtime"))
   .configs(Benchmark)
   .settings(
@@ -1164,7 +1193,6 @@ lazy val runtime = (project in file("engine/runtime"))
       sbt.internal.util.CustomLogManager.excludeMsg("Could not determine source for class ", Level.Warn),
     version := ensoVersion,
     commands += WithDebugCommand.withDebug,
-    cleanInstruments := FixInstrumentsGeneration.cleanInstruments.value,
     inConfig(Compile)(truffleRunOptionsSettings),
     inConfig(Benchmark)(Defaults.testSettings),
     inConfig(Benchmark)(
@@ -1197,13 +1225,8 @@ lazy val runtime = (project in file("engine/runtime"))
       "junit"               % "junit"                 % "4.12"            % Test,
       "com.novocode"        % "junit-interface"       % "0.11"            % Test exclude ("junit", "junit-dep")
     ),
-    // Note [Unmanaged Classpath]
-    Test / unmanagedClasspath += (baseDirectory.value / ".." / ".." / "app" / "gui" / "view" / "graph-editor" / "src" / "builtin" / "visualization" / "native" / "inc"),
     Compile / compile / compileInputs := (Compile / compile / compileInputs)
       .dependsOn(CopyTruffleJAR.preCompileTask)
-      .value,
-    Compile / compile := FixInstrumentsGeneration.patchedCompile
-      .dependsOn(FixInstrumentsGeneration.preCompileTask)
       .value,
     // Note [Classpath Separation]
     Test / javaOptions ++= Seq(
@@ -1258,20 +1281,7 @@ lazy val runtime = (project in file("engine/runtime"))
     }.evaluated,
     Benchmark / parallelExecution := false
   )
-  .settings(
-    assembly / assemblyJarName := "runtime.jar",
-    assembly / test := {},
-    assembly / assemblyOutputPath := file("runtime.jar"),
-    assembly / assemblyMergeStrategy := {
-      case PathList("META-INF", file, xs @ _*) if file.endsWith(".DSA") =>
-        MergeStrategy.discard
-      case PathList("META-INF", file, xs @ _*) if file.endsWith(".SF") =>
-        MergeStrategy.discard
-      case PathList("META-INF", "MANIFEST.MF", xs @ _*) =>
-        MergeStrategy.discard
-      case _ => MergeStrategy.first
-    }
-  )
+  .dependsOn(`runtime-language-epb`)
   .dependsOn(`edition-updater`)
   .dependsOn(`interpreter-dsl`)
   .dependsOn(`library-manager`)
@@ -1287,6 +1297,66 @@ lazy val runtime = (project in file("engine/runtime"))
   .dependsOn(syntax.jvm)
   .dependsOn(`docs-generator`)
   .dependsOn(testkit % Test)
+
+lazy val `runtime-instrument-id-execution` = (project in file("engine/runtime-instrument-id-execution"))
+  .settings(
+    inConfig(Compile)(truffleRunOptionsSettings),
+    instrumentationSettings
+  )
+  .dependsOn(runtime)
+
+lazy val `runtime-instrument-repl-debugger` = (project in file("engine/runtime-instrument-repl-debugger"))
+  .settings(
+    inConfig(Compile)(truffleRunOptionsSettings),
+    instrumentationSettings
+  )
+  .dependsOn(runtime)
+
+lazy val `runtime-instrument-runtime-server` = (project in file("engine/runtime-instrument-runtime-server"))
+  .settings(
+    inConfig(Compile)(truffleRunOptionsSettings),
+    instrumentationSettings
+  )
+  .dependsOn(runtime)
+
+lazy val `runtime-with-instruments`  = (project in file("engine/runtime-with-instruments"))
+  .configs(Benchmark)
+  .settings(
+    inConfig(Compile)(truffleRunOptionsSettings),
+    inConfig(Benchmark)(Defaults.testSettings),
+    Benchmark / javacOptions --= Seq("-source", frgaalSourceLevel),
+    Test / javaOptions ++= Seq(
+      "-Dgraalvm.locatorDisabled=true",
+      s"--upgrade-module-path=${file("engine/runtime/build-cache/truffle-api.jar").absolutePath}"
+    ),
+    Test / fork := true,
+    Test / envVars ++= distributionEnvironmentOverrides ++ Map(
+      "ENSO_TEST_DISABLE_IR_CACHE" -> "false"
+    ),
+    libraryDependencies ++= Seq(
+      "org.scalatest"      %% "scalatest"             % scalatestVersion  % Test,
+    ),
+    // Note [Unmanaged Classpath]
+    Test / unmanagedClasspath += (baseDirectory.value / ".." / ".." / "app" / "gui" / "view" / "graph-editor" / "src" / "builtin" / "visualization" / "native" / "inc"),
+    assembly / assemblyJarName := "runtime.jar",
+    assembly / test := {},
+    assembly / assemblyOutputPath := file("runtime.jar"),
+    assembly / assemblyMergeStrategy := {
+      case PathList("META-INF", file, xs @ _*) if file.endsWith(".DSA") =>
+        MergeStrategy.discard
+      case PathList("META-INF", file, xs @ _*) if file.endsWith(".SF") =>
+        MergeStrategy.discard
+      case PathList("META-INF", "MANIFEST.MF", xs @ _*) =>
+        MergeStrategy.discard
+      case PathList("META-INF", "services", xs @ _*) =>
+        MergeStrategy.concat
+      case _ => MergeStrategy.first
+    }
+  )
+  .dependsOn(runtime % "compile->compile;test->test")
+  .dependsOn(`runtime-instrument-id-execution`)
+  .dependsOn(`runtime-instrument-repl-debugger`)
+  .dependsOn(`runtime-instrument-runtime-server`)
 
 /* Note [Unmanaged Classpath]
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -1346,7 +1416,7 @@ lazy val `engine-runner` = project
   )
   .settings(
     assembly := assembly
-      .dependsOn(runtime / assembly)
+      .dependsOn(`runtime-with-instruments` / assembly)
       .value
   )
   .dependsOn(`version-output`)

engine/runtime-instrument-id-execution/src/main/java/org/enso/interpreter/instrument/IdExecutionInstrument.java
@@ -31,11 +31,9 @@ import java.util.function.Consumer;
 
 /** An instrument for getting values from AST-identified expressions. */
 @TruffleInstrument.Registration(
-    id = IdExecutionInstrument.INSTRUMENT_ID,
-    services = IdExecutionInstrument.class)
-public class IdExecutionInstrument extends TruffleInstrument {
-  public static final String INSTRUMENT_ID = "id-value-extractor";
-
+    id = IdExecutionService.INSTRUMENT_ID,
+    services = IdExecutionService.class)
+public class IdExecutionInstrument extends TruffleInstrument implements IdExecutionService {
   private Timer timer;
   private Env env;
 
@@ -56,223 +54,9 @@ public class IdExecutionInstrument extends TruffleInstrument {
    *
    * @param timer the timer to override with
    */
+  @Override
   public void overrideTimer(Timer timer) {
     this.timer = timer;
-  }
-
-  /** A class for notifications about functions being called in the course of execution. */
-  public static class ExpressionCall {
-    private final UUID expressionId;
-    private final FunctionCallInstrumentationNode.FunctionCall call;
-
-    /**
-     * Creates an instance of this class.
-     *
-     * @param expressionId the expression id where function call was performed.
-     * @param call the actual function call data.
-     */
-    public ExpressionCall(UUID expressionId, FunctionCallInstrumentationNode.FunctionCall call) {
-      this.expressionId = expressionId;
-      this.call = call;
-    }
-
-    /** @return the id of the node performing the function call. */
-    public UUID getExpressionId() {
-      return expressionId;
-    }
-
-    /** @return the function call metadata. */
-    public FunctionCallInstrumentationNode.FunctionCall getCall() {
-      return call;
-    }
-  }
-
-  /** A class for notifications about identified expressions' values being computed. */
-  public static class ExpressionValue {
-    private final UUID expressionId;
-    private final Object value;
-    private final String type;
-    private final String cachedType;
-    private final FunctionCallInfo callInfo;
-    private final FunctionCallInfo cachedCallInfo;
-    private final ProfilingInfo[] profilingInfo;
-    private final boolean wasCached;
-
-    /**
-     * Creates a new instance of this class.
-     *
-     * @param expressionId the id of the expression being computed.
-     * @param value the value returned by computing the expression.
-     * @param type the type of the returned value.
-     * @param cachedType the cached type of the value.
-     * @param callInfo the function call data.
-     * @param cachedCallInfo the cached call data.
-     * @param profilingInfo the profiling information associated with this node
-     * @param wasCached whether or not the value was obtained from the cache
-     */
-    public ExpressionValue(
-        UUID expressionId,
-        Object value,
-        String type,
-        String cachedType,
-        FunctionCallInfo callInfo,
-        FunctionCallInfo cachedCallInfo,
-        ProfilingInfo[] profilingInfo,
-        boolean wasCached) {
-      this.expressionId = expressionId;
-      this.value = value;
-      this.type = type;
-      this.cachedType = cachedType;
-      this.callInfo = callInfo;
-      this.cachedCallInfo = cachedCallInfo;
-      this.profilingInfo = profilingInfo;
-      this.wasCached = wasCached;
-    }
-
-    @Override
-    public String toString() {
-      String profilingInfo = Arrays.toString(this.profilingInfo);
-      return "ExpressionValue{"
-          + "expressionId="
-          + expressionId
-          + ", value="
-          + new MaskedString(value.toString()).applyMasking()
-          + ", type='"
-          + type
-          + '\''
-          + ", cachedType='"
-          + cachedType
-          + '\''
-          + ", callInfo="
-          + callInfo
-          + ", cachedCallInfo="
-          + cachedCallInfo
-          + ", profilingInfo="
-          + profilingInfo
-          + ", wasCached="
-          + wasCached
-          + '}';
-    }
-
-    /** @return the id of the expression computed. */
-    public UUID getExpressionId() {
-      return expressionId;
-    }
-
-    /** @return the type of the returned value. */
-    public String getType() {
-      return type;
-    }
-
-    /** @return the cached type of the value. */
-    public String getCachedType() {
-      return cachedType;
-    }
-
-    /** @return the computed value of the expression. */
-    public Object getValue() {
-      return value;
-    }
-
-    /** @return the function call data. */
-    public FunctionCallInfo getCallInfo() {
-      return callInfo;
-    }
-
-    /** @return the function call data previously associated with the expression. */
-    public FunctionCallInfo getCachedCallInfo() {
-      return cachedCallInfo;
-    }
-
-    /** @return the profiling information associated with this expression */
-    public ProfilingInfo[] getProfilingInfo() {
-      return profilingInfo;
-    }
-
-    /** @return whether or not the expression result was obtained from the cache */
-    public boolean wasCached() {
-      return wasCached;
-    }
-
-    /** @return {@code true} when the type differs from the cached value. */
-    public boolean isTypeChanged() {
-      return !Objects.equals(type, cachedType);
-    }
-
-    /** @return {@code true} when the function call differs from the cached value. */
-    public boolean isFunctionCallChanged() {
-      return !Objects.equals(callInfo, cachedCallInfo);
-    }
-  }
-
-  /** Information about the function call. */
-  public static class FunctionCallInfo {
-
-    private final QualifiedName moduleName;
-    private final QualifiedName typeName;
-    private final String functionName;
-
-    /**
-     * Creates a new instance of this class.
-     *
-     * @param call the function call.
-     */
-    public FunctionCallInfo(FunctionCallInstrumentationNode.FunctionCall call) {
-      RootNode rootNode = call.getFunction().getCallTarget().getRootNode();
-      if (rootNode instanceof MethodRootNode) {
-        MethodRootNode methodNode = (MethodRootNode) rootNode;
-        moduleName = methodNode.getModuleScope().getModule().getName();
-        typeName = methodNode.getAtomConstructor().getQualifiedName();
-        functionName = methodNode.getMethodName();
-      } else if (rootNode instanceof EnsoRootNode) {
-        moduleName = ((EnsoRootNode) rootNode).getModuleScope().getModule().getName();
-        typeName = null;
-        functionName = rootNode.getName();
-      } else {
-        moduleName = null;
-        typeName = null;
-        functionName = rootNode.getName();
-      }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) {
-        return true;
-      }
-      if (o == null || getClass() != o.getClass()) {
-        return false;
-      }
-      FunctionCallInfo that = (FunctionCallInfo) o;
-      return Objects.equals(moduleName, that.moduleName)
-          && Objects.equals(typeName, that.typeName)
-          && Objects.equals(functionName, that.functionName);
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hash(moduleName, typeName, functionName);
-    }
-
-    @Override
-    public String toString() {
-      return moduleName + "::" + typeName + "::" + functionName;
-    }
-
-    /** @return the name of the module this function was defined in, or null if not available. */
-    public QualifiedName getModuleName() {
-      return moduleName;
-    }
-
-    /** @return the name of the type this method was defined for, or null if not a method. */
-    public QualifiedName getTypeName() {
-      return typeName;
-    }
-
-    /** @return the name of this function. */
-    public String getFunctionName() {
-      return functionName;
-    }
   }
 
   /** The listener class used by this instrument. */
@@ -506,6 +290,7 @@ public class IdExecutionInstrument extends TruffleInstrument {
    * @param onExceptionalCallback the consumer of the exceptional events.
    * @return a reference to the attached event listener.
    */
+  @Override
   public EventBinding<ExecutionEventListener> bind(
       CallTarget entryCallTarget,
       LocationFilter locationFilter,

engine/runtime-instrument-repl-debugger/src/main/java/org/enso/interpreter/instrument/ReplDebuggerInstrument.java
@@ -20,7 +20,6 @@ import org.enso.interpreter.node.expression.builtin.text.util.ToJavaStringNode;
 import org.enso.interpreter.node.expression.debug.CaptureResultScopeNode;
 import org.enso.interpreter.node.expression.debug.EvalNode;
 import org.enso.interpreter.runtime.Context;
-import org.enso.interpreter.runtime.builtin.Builtins;
 import org.enso.interpreter.runtime.callable.CallerInfo;
 import org.enso.interpreter.runtime.callable.function.Function;
 import org.enso.interpreter.runtime.data.text.Text;
@@ -61,8 +60,8 @@ public class ReplDebuggerInstrument extends TruffleInstrument {
         instrumenter.attachExecutionEventFactory(
             filter,
             ctx ->
-                new ReplExecutionEventNode(
-                    ctx, handler, env.getLogger(ReplExecutionEventNode.class)));
+                new ReplExecutionEventNodeImpl(
+                    ctx, handler, env.getLogger(ReplExecutionEventNodeImpl.class)));
       } else {
         env.getLogger(ReplDebuggerInstrument.class)
             .warning("ReplDebuggerInstrument was initialized, " + "but no client connected");
@@ -85,7 +84,8 @@ public class ReplDebuggerInstrument extends TruffleInstrument {
   }
 
   /** The actual node that's installed as a probe on any node the instrument was launched for. */
-  public static class ReplExecutionEventNode extends ExecutionEventNode {
+  private static class ReplExecutionEventNodeImpl extends ExecutionEventNode
+      implements ReplExecutionEventNode {
     private @Child EvalNode evalNode = EvalNode.buildWithResultScopeCapture();
     private @Child ToJavaStringNode toJavaStringNode = ToJavaStringNode.build();
 
@@ -95,7 +95,7 @@ public class ReplDebuggerInstrument extends TruffleInstrument {
     private DebuggerMessageHandler handler;
     private TruffleLogger logger;
 
-    private ReplExecutionEventNode(
+    private ReplExecutionEventNodeImpl(
         EventContext eventContext, DebuggerMessageHandler handler, TruffleLogger logger) {
       this.eventContext = eventContext;
       this.handler = handler;
@@ -114,11 +114,7 @@ public class ReplDebuggerInstrument extends TruffleInstrument {
       return currentFrame;
     }
 
-    /**
-     * Lists all the bindings available in the current execution scope.
-     *
-     * @return a map, where keys are variable names and values are current values of variables.
-     */
+    @Override
     public Map<String, Object> listBindings() {
       Map<String, FramePointer> flatScope =
           nodeState.getLastScope().getLocalScope().flattenBindings();
@@ -129,12 +125,7 @@ public class ReplDebuggerInstrument extends TruffleInstrument {
       return result;
     }
 
-    /**
-     * Evaluates an arbitrary expression in the current execution context.
-     *
-     * @param expression the expression to evaluate
-     * @return the result of evaluating the expression or an exception that caused failure
-     */
+    @Override
     public Either<Exception, Object> evaluate(String expression) {
       ReplExecutionEventNodeState savedState = nodeState;
       try {
@@ -155,14 +146,11 @@ public class ReplDebuggerInstrument extends TruffleInstrument {
       }
     }
 
-    /**
-     * Returns the String representation of the provided object as defined by Enso {@code to_text}
-     * operation.
-     */
-    public Either<Exception, String> showObject(Object o) {
+    @Override
+    public Either<Exception, String> showObject(Object object) {
       try {
         InteropLibrary interop = InteropLibrary.getUncached();
-        return new Right<>(interop.asString(interop.toDisplayString(o)));
+        return new Right<>(interop.asString(interop.toDisplayString(object)));
       } catch (Exception e) {
         return new Left<>(e);
       }
@@ -176,15 +164,7 @@ public class ReplDebuggerInstrument extends TruffleInstrument {
       }
     }
 
-    /**
-     * Terminates this REPL session.
-     *
-     * <p>The last result of {@link #evaluate(String)} (or {@link Builtins#nothing()} if {@link
-     * #evaluate(String)} was not called before) will be returned from the instrumented node.
-     *
-     * <p>This function must always be called at the end of REPL session, as otherwise the program
-     * will never resume. It's forbidden to use this object after exit has been called.
-     */
+    @Override
     public void exit() {
       throw eventContext.createUnwind(nodeState.getLastReturn());
     }

engine/runtime-language-epb/src/main/java/org/enso/interpreter/epb/EpbContext.java
@@ -3,7 +3,6 @@ package org.enso.interpreter.epb;
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.TruffleLanguage;
 import com.oracle.truffle.api.nodes.Node;
-import org.enso.interpreter.Language;
 import org.enso.interpreter.epb.runtime.GuardedTruffleContext;
 
 /**

junglerot/jungle

commit 45b1c7e04385d59f8cf6bf548448f554aa5ffe02

Fix runtime-with-instruments dependency on runtime (#3529)
This change makes sure that Runtime configuration of `runtime` is listed
as a dependency of `runtime-with-instruments`.
That way `buildEngineDistribution` which indirectly depends on
`runtime-with-instruments`/assembly triggers compilation for std-bits,
if necessary.

# Important Notes
Minor adjustments for a problem introduced in https://github.com/enso-org/enso/pull/3509

build.sbt
@@ -1353,7 +1353,7 @@ lazy val `runtime-with-instruments`  = (project in file("engine/runtime-with-ins
       case _ => MergeStrategy.first
     }
   )
-  .dependsOn(runtime % "compile->compile;test->test")
+  .dependsOn(runtime % "compile->compile;test->test;runtime->runtime")
   .dependsOn(`runtime-instrument-id-execution`)
   .dependsOn(`runtime-instrument-repl-debugger`)
   .dependsOn(`runtime-instrument-runtime-server`)