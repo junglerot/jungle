junglerot/jungle

commit 642819137605d94bba545d0d0e45cc9b6948aadc

Ensure Table Visualisation is the Default Visualization for Tables (#4120)
Adds a mechanism for the GUI to choose an appropriate default visualization for a newly created node.

https://user-images.githubusercontent.com/1428930/216641714-2410d2c9-44d2-45f0-856d-1f995421c94b.mp4

# Important Notes
This is a workaround for [missing functionality](https://www.pivotaltracker.com/story/show/184390437) that would take much longer to implement.

app/gui/view/graph-editor/src/component/visualization/container.rs
@@ -499,6 +499,13 @@ impl Container {
         frp::extend! { network
             eval  frp.set_visibility    ((v) model.set_visibility(*v));
             eval_ frp.toggle_visibility (model.toggle_visibility());
+
+            visualisation_uninitialised <- frp.set_visualization.map(|t| t.is_none());
+            set_default_visualisation <- frp.set_vis_input_type.gate(&visualisation_uninitialised).unwrap();
+            default_visualisation <- set_default_visualisation.map(f!((tp) {
+               registry.default_visualization_for_type(tp)
+            }));
+
             eval  frp.set_data          ((t) model.set_visualization_data(t));
             frp.source.size    <+ frp.set_size;
             frp.source.visible <+ frp.set_visibility;
@@ -524,7 +531,7 @@ impl Container {
         // === Switching Visualizations ===
 
         frp::extend! { network
-            new_vis_definition <- any(frp.set_visualization,vis_after_cycling);
+            new_vis_definition <- any(frp.set_visualization,vis_after_cycling,default_visualisation);
             let preprocessor   =  &frp.source.preprocessor;
             frp.source.visualisation <+ new_vis_definition.map(f!(
                 [model,action_bar,app,preprocessor](vis_definition) {
@@ -661,7 +668,7 @@ impl Container {
         // This is not optimal the optimal solution to this problem, as it also means that we have
         // an animation on an invisible component running.
         frp.set_size.emit(Vector2(DEFAULT_SIZE.0, DEFAULT_SIZE.1));
-        frp.set_visualization.emit(Some(visualization::Registry::default_visualisation()));
+        frp.set_visualization.emit(None);
         self
     }
 

app/gui/view/graph-editor/src/builtin/visualization/java_script/table.js
@@ -11,7 +11,12 @@ loadStyleFromString(scrollbarStyle)
 // ===========================
 
 class TableVisualization extends Visualization {
-    static inputType = 'Any'
+    // IMPORTANT: When updating this, also update the test in
+    // test/Visualization_Tests/src/Default_Visualizations_Spec.enso:15 as this verifies that the
+    // type names do not go out of sync. Should be removed once
+    // https://github.com/enso-org/enso/issues/5195 is implemented.
+    static inputType =
+        'Standard.Table.Data.Table.Table | Standard.Table.Data.Column.Column | Standard.Base.Data.Vector.Vector | Standard.Base.Data.Array.Array | Any '
     static label = 'Table'
 
     constructor(data) {

app/gui/view/graph-editor/src/component/visualization/registry.rs
@@ -76,6 +76,19 @@ impl Registry {
         result
     }
 
+    /// Return the `visualization::Definition` that should be used as default for the given type.
+    pub fn default_visualization_for_type(
+        &self,
+        tp: &enso::Type,
+    ) -> Option<visualization::Definition> {
+        // TODO[MM]: Visualisations are order by "matching the type" first, followed by and then
+        // "matching any type". So we just take the first one, which should be the most appropriate
+        // one. This should be replaced with the proper solution described in
+        // https://github.com/enso-org/enso/issues/5195
+        let valid_sources = self.valid_sources(tp);
+        valid_sources.into_iter().next()
+    }
+
     /// Return the `visualization::Definition` registered for the given `visualization::Path`.
     pub fn definition_from_path(
         &self,
@@ -92,15 +105,19 @@ impl Registry {
 
     /// Add default visualizations to the registry.
     pub fn add_default_visualizations(&self) {
+        // Note that the order is important. Visualisations that are added first will be
+        // prioritised as default (as long as they have a matching type to the value they will
+        // represent.
         self.add(builtin::visualization::native::text_visualization::text_visualisation());
+        self.try_add_java_script(builtin::visualization::java_script::table_visualization());
         self.try_add_java_script(builtin::visualization::java_script::scatter_plot_visualization());
         self.try_add_java_script(builtin::visualization::java_script::histogram_visualization());
         self.try_add_java_script(builtin::visualization::java_script::heatmap_visualization());
-        self.try_add_java_script(builtin::visualization::java_script::table_visualization());
         self.try_add_java_script(builtin::visualization::java_script::sql_visualization());
         self.try_add_java_script(builtin::visualization::java_script::geo_map_visualization());
         self.try_add_java_script(builtin::visualization::java_script::image_base64_visualization());
         self.try_add_java_script(builtin::visualization::java_script::warnings_visualization());
+        self.add(builtin::visualization::native::text_visualization::text_visualisation());
     }
 
     /// Return a default visualisation definition.

test/Visualization_Tests/src/Default_Visualizations_Spec.enso
@@ -1,0 +1,24 @@
+from Standard.Base import all
+
+from Standard.Test import Test
+import Standard.Test.Extensions
+from Standard.Table import Table
+
+sample_table = Table.new [["A", [1, 2, 3]]]
+sample_column = sample_table.get "A"
+
+spec =
+    expect_type_representation value expected_text =
+            type_name = Meta.get_qualified_type_name value . to_text
+            type_name . should_equal <| expected_text
+
+    Test.group "Type Names of Visualisation Defaults" <|
+        """ IMPORTANT: When updating this, also update the default values in
+             app/gui/view/graph-editor/src/builtin/visualization/java_script/table.js:18 as this
+             verifies that the type names do not go out of sync. Should be removed once
+             https://github.com/enso-org/enso/issues/5195 is implemented.
+        Test.specify "Type names should match table visualization expectations" <|
+            expect_type_representation [1,2] "Standard.Base.Data.Vector.Vector"
+            expect_type_representation [1,2].to_array "Standard.Base.Data.Array.Array"
+            expect_type_representation sample_table "Standard.Table.Data.Table.Table"
+            expect_type_representation sample_column "Standard.Table.Data.Column.Column"

test/Visualization_Tests/src/Main.enso
@@ -2,6 +2,7 @@ from Standard.Base import all
 
 from Standard.Test import Test_Suite
 
+import project.Default_Visualizations_Spec
 import project.Geo_Map_Spec
 import project.Helpers_Spec
 import project.Histogram_Spec
@@ -14,6 +15,7 @@ import project.Visualization_Spec
 import project.Widgets_Spec
 
 main = Test_Suite.run_main <|
+    Default_Visualizations_Spec.spec
     Geo_Map_Spec.spec
     Helpers_Spec.spec
     Histogram_Spec.spec

commit 930913e1ec1917af77e1e3937c8d05adda1b3831

Fix missing visualization preview in CB (#5757)
Closes #5639

This PR fixes a regression introduced in #4120, due to which new nodes that were being edited had an empty visualization preview. Now newly created visualization containers do get a default visualization set even before the visualization's input type is set.

https://user-images.githubusercontent.com/117099775/221256294-c87a50da-c8b0-4f00-bf84-f5ae551fca72.mp4

app/gui/view/graph-editor/src/component/visualization/container.rs
@@ -501,10 +501,14 @@ impl Container {
             eval_ frp.toggle_visibility (model.toggle_visibility());
 
             visualisation_uninitialised <- frp.set_visualization.map(|t| t.is_none());
-            set_default_visualisation <- frp.set_vis_input_type.gate(&visualisation_uninitialised).unwrap();
-            default_visualisation <- set_default_visualisation.map(f!((tp) {
+            default_visualisation <- visualisation_uninitialised.on_true().map(|_| {
+                Some(visualization::Registry::default_visualisation())
+            });
+            vis_input_type <- frp.set_vis_input_type.gate(&visualisation_uninitialised).unwrap();
+            default_visualisation_for_type <- vis_input_type.map(f!((tp) {
                registry.default_visualization_for_type(tp)
             }));
+            default_visualisation <- any(&default_visualisation, &default_visualisation_for_type);
 
             eval  frp.set_data          ((t) model.set_visualization_data(t));
             frp.source.size    <+ frp.set_size;